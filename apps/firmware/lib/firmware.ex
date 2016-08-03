18d9a8385c4d400610b9e19c201492efcc12322a
Joel Byler <jbyler@covermymeds.com>
2016-08-03 14:19:38 -0400

    WIP - spike out linux deps

--- apps/firmware/lib/firmware.ex
+++ apps/firmware/lib/firmware.ex
@@ -8,10 +8,6 @@ defmodule Firmware do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    unless :os.type == {:unix, :darwin} do     # don't start networking unless we're on nerves
-      {:ok, _pid} = Networking.setup :eth0
-    end
-
     migrate
     seed
 
@@ -58,6 +54,40 @@ defmodule Firmware do
       description: "Its good to make friends",
       bonus: false
     }
+  end
+
+  defp setup_network do
+
+    IO.puts "Setting sysctl"
+    System.cmd("sysctl", ["-w", "net.ipv4.ip_forward=1"]) |> print_cmd_result
+
+    IO.puts "Initializing eth0"
+    Networking.setup(:eth0)
+
+    System.cmd("ip", ["link", "set", "eth0", "up"]) |> print_cmd_result
+    System.cmd("ip", ["addr", "add", "192.168.1.6/24", "dev", "eth0"]) |> print_cmd_result
+    System.cmd("ip", ["route", "add", "default", "via", "192.168.1.1"]) |> print_cmd_result
+
+    IO.puts "Initializing wlan0"
+    System.cmd("ip", ["link", "set", "wlan0", "up"]) |> print_cmd_result
+    System.cmd("ip", ["addr", "add", "192.168.24.1/24", "dev", "wlan0"]) |> print_cmd_result
+
+    IO.puts "Initializing dnsmasq"
+    System.cmd("dnsmasq", ["--dhcp-lease", "/root/dnsmasq.lease"]) |> print_cmd_result
+
+    IO.puts "Initializing system"
+    System.cmd("hostapd", ["-B", "-d", "/etc/hostapd/hostapd.conf"]) |> print_cmd_result
+
+    IO.puts "Initializing iptables"
+    System.cmd("setup_iptables", []) |> print_cmd_result
+  end
+
+  defp print_cmd_result({message, 0}) do
+    IO.puts message
+  end
+
+  defp print_cmd_result({message, err_no}) do
+    IO.puts "ERROR (#{err_no}): #{message}"
   end
 
 end
