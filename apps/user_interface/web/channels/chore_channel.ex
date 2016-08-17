7692530f9ecdec7b1008afacd43338728b33506d
Joel Byler <jbyler@covermymeds.com>
2016-08-17 16:34:31 -0400

    Cleanup cruft

--- apps/user_interface/web/channels/chore_channel.ex
+++ apps/user_interface/web/channels/chore_channel.ex
@@ -8,27 +8,8 @@ defmodule UserInterface.ChoreChannel do
   end
 
   def handle_info(:after_join, socket) do
-    {:ok, _} = Presence.track(socket, socket.assigns.mac, %{
-      status: "online"
-    })
-    IO.puts(Map.keys(Presence.list(socket)))
+    {:ok, _} = Presence.track(socket, socket.assigns.mac, %{ status: "online" })
     push socket, "presence_state", Presence.list(socket)
-    {:noreply, socket}
-  end
-
-  def handle_in("new_msg", %{"body" => body}, socket) do
-    broadcast! socket, "new_msg", %{
-      body: body,
-      author: socket.assigns.mac,
-    }
-
-    {:noreply, socket}
-  end
-
-  def handle_in("new_status", %{"status" => status}, socket) do
-    {:ok, _} = Presence.update(socket, socket.assigns.name, %{
-      status: status
-    })
     {:noreply, socket}
   end
 end
