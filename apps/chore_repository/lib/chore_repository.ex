2f0bfae9c40aee4ccf81380333382ad0dec6de49
Joel Byler <jbyler@covermymeds.com>
2016-08-20 22:17:48 -0400

    put kids chores back for practice

--- apps/chore_repository/lib/chore_repository.ex
+++ apps/chore_repository/lib/chore_repository.ex
@@ -99,20 +99,19 @@ defmodule ChoreRepository do
 #   end
 
   def seed do
-    insert("Attend Joel's Talk", "1", "I think you'll enjoy it.", "true")
-    insert("Tweet at Joel", "2", "His twittler account is @joelbyler")
-    insert("Complement your neighbor", "3", "Its good to make friends")
-    # done("aa:bb:cc:dd:ee:ff", "192.168.24.11")
+    # insert("Attend Joel's Talk", "1", "I think you'll enjoy it.", "true")
+    # insert("Tweet at Joel", "2", "His twittler account is @joelbyler")
+    # insert("Complement your neighbor", "3", "Its good to make friends")
 
-    # insert("Make your bed", "1", "So it looks nice and you have a place to sit", "true")
-    # insert("Exercise", "2", "Do 25 jumping jacks, to help keep fit")
-    # insert("Clean your room", "3", "Keep it nice and clean so you have more room to play", "true")
-    # insert("Give your mom a hug", "4", "Maybe even a kiss, to show her how you love her")
-    # insert("Put your clothes away", "5", "A place for everything and everything in its place", "true")
-    # insert("Tell your sister you lover her", "6", "Its good to show others how you feel")
-    # insert("Tidy up your closet", "7", "Its hard to find anything when its a mess in there", "true")
-    # insert("Give the pets some attention", "8", "Olaf and Chleo need love too")
-    # insert("Put away dirty clothes", "9", "Put them in the hamper so your mother can wash them", "true")
+    insert("Make your bed", "1", "So it looks nice and you have a place to sit", "true")
+    insert("Exercise", "2", "Do 25 jumping jacks or 10 pushups, to help keep fit")
+    insert("Clean your room", "3", "Keep it nice and clean so you have more room to play", "true")
+    insert("Give your mom a hug", "4", "Maybe even a kiss, to show her how you love her")
+    insert("Put your clothes away", "5", "A place for everything and everything in its place", "true")
+    insert("Tell your sister you lover her", "6", "Its good to show others how you feel")
+    insert("Tidy up your closet", "7", "Its hard to find anything when its a mess in there", "true")
+    insert("Give the pets some attention", "8", "They need love too")
+    insert("Put away dirty clothes", "9", "Put them in the hamper so your mother can wash them", "true")
 
   end
 
